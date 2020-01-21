71209bb3fbf97d04e39df44c7fd5c70b1bb161e5
Yoshi Akutsu <akutsu.yoshi@gmail.com>
2020-01-21 14:02:08 -0500

    added basic tic tac toe board and empty factory function and modules

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-script
+const gameBoard = (() => {
+    const startGame = () => {
+        let arrayBoard = [];
+    };
+    const findWinner = (arrayBoard) =>{
+
+    };
+    return {startGame, findWinner};
+})();
+
+const displayController = (() =>{
+    const addMark = (player) => {
+
+    };
+    const displayWinner = () => {}
+    return {addMark, displayWinner}
+})();
+
+const createPlayer = (player) => {
+    let name = player;
+    const chooseMark = 
+    const makeMove = () => {
+        
+    }
+    return {makeMove};
+}
